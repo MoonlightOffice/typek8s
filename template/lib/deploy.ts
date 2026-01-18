686fe4f28912fadeef9f8a7c8986fb4fb5a19695
Moon <moonlightoffice@example.com>
2026-01-19 00:59:23 +0900

    feat(template): add example usage

---
+++ template/lib/deploy.ts
@@ -0,0 +1,61 @@
+// Define your apps whatever you like. You can delete all this file.
+
+import { api } from "typek8s";
+
+function deploy(name: string, image: string): api.appsV1["Deployment"] {
+  return {
+    apiVersion: "apps/v1",
+    kind: "Deployment",
+    metadata: {
+      name: name,
+      labels: {
+        "app.kubernetes.io/name": name,
+      },
+    },
+    spec: {
+      strategy: {
+        type: "RollingUpdate",
+        rollingUpdate: {
+          maxSurge: "25%",
+          maxUnavailable: "25%",
+        },
+      },
+      replicas: 3,
+      selector: {
+        matchLabels: {
+          "deploy": name,
+        },
+      },
+      template: {
+        metadata: {
+          labels: {
+            "deploy": name,
+          },
+        },
+        spec: {
+          containers: [
+            {
+              name: name,
+              image: image,
+              imagePullPolicy: "IfNotPresent",
+              resources: {
+                requests: {
+                  "memory": "100Mi",
+                  "cpu": 0.1,
+                },
+                limits: {
+                  "memory": "100Mi",
+                  "cpu": 0.1,
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+  };
+}
+
+export const deployments: api.appsV1["Deployment"][] = [
+  deploy("file-server", "caddy:latest"),
+];
---
+++ template/main.ts
@@ -0,0 +1,12 @@
+import { TypeK8s } from "typek8s";
+import { deployments } from "~/lib/deploy.ts";
+
+TypeK8s.synth({
+  manifests: [
+    ...deployments,
+  ],
+}, {
+  type: "chart",
+  chartName: "sample-ap",
+  outputDir: "./dist/",
+});
