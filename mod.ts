d8e647f437770cf993a5e1137785b4228fe96695
Moon <moonlightoffice@example.com>
2026-04-19 21:42:10 +0900

    feat(pkg): export type Chart and Manifest

--- mod.ts
+++ mod.ts
@@ -1,4 +1,7 @@
-import { di, service } from "./pkg/deps.ts"
+import { di, entity, service } from "./pkg/deps.ts"
 
 export type SynthParams = service.synth.SynthParams
 export const SynthService = di.createSynthService()
+
+export type Chart = entity.Chart
+export type anifest = entity.Manifest
